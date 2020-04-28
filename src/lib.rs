b9cd045849b0069ac9cf8dc5716b374acdd467c3
SpiralP <spiral.palmer@gmail.com>
2020-04-27 22:09:26 -0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use classicube_sys::*;
+use std::{os::raw::c_int, ptr};
+
+extern "C" fn init() {
+    //
+}
+
+extern "C" fn free() {
+    //
+}
+
+extern "C" fn on_new_map_loaded() {
+    //
+}
+
+#[no_mangle]
+pub static Plugin_ApiVersion: c_int = 1;
+
+#[no_mangle]
+pub static mut Plugin_Component: IGameComponent = IGameComponent {
+    // Called when the game is being loaded.
+    Init: Some(init),
+    // Called when the component is being freed. (e.g. due to game being closed)
+    Free: Some(free),
+    // Called to reset the component's state. (e.g. reconnecting to server)
+    Reset: None,
+    // Called to update the component's state when the user begins loading a new map.
+    OnNewMap: None,
+    // Called to update the component's state when the user has finished loading a new map.
+    OnNewMapLoaded: Some(on_new_map_loaded),
+    // Next component in linked list of components.
+    next: ptr::null_mut(),
+};
